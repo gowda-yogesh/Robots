e2501a700fcd0742733d2678a2664b6f27f0853d
YOGESH C GOWDA <yog.161096@gmail.com>
2020-08-07 20:18:27 +0530

    Updates

---
+++ precache-manifest.eb42b6b136518811f0be2e284183e442.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4eec58abcb176b4c3b3d932f6c752931",
+    "url": "/Robots/index.html"
+  },
+  {
+    "revision": "b9c15b36bf34efd1037d",
+    "url": "/Robots/static/css/main.0f845b25.chunk.css"
+  },
+  {
+    "revision": "cc5a421e55494d401d60",
+    "url": "/Robots/static/js/2.327ff6f8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Robots/static/js/2.327ff6f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9c15b36bf34efd1037d",
+    "url": "/Robots/static/js/main.9389aadd.chunk.js"
+  },
+  {
+    "revision": "3eb3c4e75aa3eeceb013",
+    "url": "/Robots/static/js/runtime-main.8ce48907.js"
+  }
+]);
